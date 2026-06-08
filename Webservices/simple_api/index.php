96de87cfa35374667a0dd0c3f800899d13017587
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2026-06-08 17:59:44 +0530

    first commit

---
+++ Webservices/simple_api/index.php
@@ -0,0 +1,25 @@
+<?php
+// data read or fetch API
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
+require_once "dbconfig.php";
+$query = "SELECT * FROM tbl_product";
+$result = mysqli_query($conn, $query);
+$count = mysqli_num_rows($result);
+
+if($count > 0)
+{	
+	$output_arr = mysqli_fetch_all($result, MYSQLI_ASSOC); // get all data in arr
+	echo json_encode($output_arr);
+}
+else
+{	
+	
+	echo json_encode(array("message" => "No Product Found.", "status" => false));
+}
+
+?>
